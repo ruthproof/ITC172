2b9969da8164ca958540651bbda34142828f2dde
Ruth Mansoor <ruth.improves@gmail.com>
2016-05-19 12:25:12 -0700

    Commit App code

---
+++ SelectService/app_code/IShowService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShowService" in both code and config file together.
+[ServiceContract]
+public interface IShowService
+{
+    [OperationContract]
+    List<string> getArtist();
+
+    [OperationContract]
+    List<string> getVenues();
+
+    [OperationContract]
+    List<ShowInfo> showsByVenues(string venueName);
+    [OperationContract]
+    List<ShowInfo> getShowsByArtist(string artistName);
+    
+}
+
+[DataContract]
+public class ShowInfo
+{
+    [DataMember]
+    public string ShowName {get; set;}
+    [DataMember]
+    public string ShowDate { get; set; }
+    [DataMember]
+    public string ShowStartTime { get; set; }
+
+}
+public class ArtistInfo
+{
+    [DataMember]
+    public string ShowName { get; set; }
+    [DataMember]
+    public string ShowDate { get; set; }
+    [DataMember]
+    public string VenueName { get; set; }
+}
---
+++ SelectService/app_code/ShowService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ShowService" in code, svc and config file together.
+public class ShowService : IShowService
+{
+
+    ShowTrackerEntities db = new ShowTrackerEntities();
+    //list all venues *CHECK*
+    //list all artists *CHECK*
+    //list all shows
+    //List all shows for Venue(ShowName, ShowDate, ShowTime)
+    //List all shows for Artist(ShowName, ShowDate, ShowTime, AristName)
+    //test each method to show if it fails or not
+    public List<string> getArtist()
+    {
+        var art = from a in db.Artists
+                  orderby a.ArtistName
+                  select new { a.ArtistName };
+
+        List<string> artists = new List<string>();
+        foreach(var ar in art)
+        {
+            artists.Add(ar.ArtistName);
+        }
+
+        return artists;
+    }
+
+ 
+
+    public List<string> getVenues()
+    {
+        
+            var VenueNames = from a in db.Venues
+                      orderby a.VenueName
+                      select new { a.VenueName };
+
+            List<string> venue = new List<string>();
+            foreach (var ve in VenueNames)
+            {
+                venue.Add(ve.VenueName);
+            }
+
+            return venue;
+        
+        //throw new NotImplementedException();
+    }
+
+    public List<ShowInfo> showsByVenues(string venueName)
+    {
+        List<ShowInfo> venues = new List<ShowInfo>();
+        var vens = from v in db.Venues
+                   from s in v.Shows
+                   where v.VenueName == venueName
+                   select new
+                   {
+                       s.ShowName,
+                       s.ShowDate,
+                       s.ShowTime
+                   };
+        List<ShowInfo> VenueShow = new List<ShowInfo>();
+        foreach (var ve in vens)
+            {
+            ShowInfo info = new ShowInfo();
+            info.ShowName = ve.ShowName;
+            info.ShowDate = ve.ShowDate.ToString();
+            info.ShowStartTime = ve.ShowTime.ToString();
+            VenueShow.Add(info);
+            }
+
+            return VenueShow;
+        }
+
+    public List<ShowInfo> getShowsByArtist(string artistName)
+    {
+        List<ShowInfo> artists = new List<ShowInfo>();
+        var arts = from a in db.Shows
+                   from sd in a.ShowDetails
+                   where sd.Artist.ArtistName == artistName
+                   select new
+                   {
+                       a.ShowName,
+                       a.ShowDate,
+                       a.ShowTime
+                   };
+        List<ShowInfo> ArtistShow = new List<ShowInfo>();
+        foreach (var ar in arts)
+        {
+            ShowInfo info = new ShowInfo();
+            info.ShowName = ar.ShowName;
+            info.ShowDate = ar.ShowDate.ToString();
+            info.ShowStartTime = ar.ShowTime.ToString();
+            ArtistShow.Add(info);
+        }
+
+        return ArtistShow;
+    }
+
+
+}
+
