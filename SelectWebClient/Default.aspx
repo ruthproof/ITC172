c15c8162a4bc233d09d7ea8c1c9389c272221045
Ruth Mansoor <ruth.improves@gmail.com>
2016-05-26 12:20:33 -0700

    Commit!

---
+++ SelectWebClient/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="ArtistDropDownList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ArtistDropDownList_SelectedIndexChanged" style="height: 22px"></asp:DropDownList>
+    </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:DropDownList ID="VenueDropDownList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="VenueDropDownList_SelectedIndexChanged">
+        </asp:DropDownList>
+    </form>
+</body>
+</html>
---
+++ SelectWebClient/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ServiceReference1.ShowServiceClient sc = new ServiceReference1.ShowServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FillArtistDropDown();
+            GetVenues();
+        }
+    }
+
+    protected void FillArtistDropDown()
+    {
+        string[] artist = sc.getArtist();
+        ArtistDropDownList.DataSource = artist;
+        ArtistDropDownList.DataBind();
+    }
+
+    protected void ArtistDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        GetShowByArtist();
+    }
+
+    protected void GetShowByArtist()
+    {
+        string artname = ArtistDropDownList.SelectedItem.Text;
+        ServiceReference1.ShowInfo[] show = sc.getShowsByArtist(artname);
+        GridView1.DataSource = show;
+        GridView1.DataBind();
+    }
+
+   
+
+    protected void GetVenues()
+    {
+        string[] venue = sc.getVenues();
+        VenueDropDownList.DataSource = venue;
+        VenueDropDownList.DataBind();
+    }
+
+    protected void VenueDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string venuename = VenueDropDownList.SelectedItem.Text;
+        ServiceReference1.ShowInfo[] show = sc.showsByVenues(venuename);
+        GridView1.DataSource = show;
+        GridView1.DataBind();
+    }
+}
+
